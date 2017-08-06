8e6f29533a168bee8a222cdd4ad705610fa1a44e
john <john@swarmee.net>
2017-08-06 14:13:11 +1000

    sunday afternoon

--- fastest-humans-over-100m/fastest-humans-over-100m.sh
+++ fastest-humans-over-100m/fastest-humans-over-100m.sh
@@ -1,8 +1,8 @@
-echo '################ delete index '"$2"' on '"$1"'###############'
+echo '################ delete index fastest-humans-over-100m-'"$2"' on '"$1"'###############'
 curl -XDELETE ''"$1"':9200/fastest-humans-over-100m-'"$2"'?pretty'
 echo '################ run load ###################'
 zcat fastest-humans-over-100m.csv.gz  |  HOST=$1 INDEX=$2 /usr/share/logstash/bin/logstash -f fastest-humans-over-100m.conf -b 1 -w 1
-echo '################ Remove Any Old Aliases for fastest-humanes-over-100ms And Creates a New One for '"$2"' ###############'
+echo '################ Remove Any Old Aliases for fastest-humanes-over-100ms And Creates a New One for fastest-humans-over-100m-'"$2"' ###############'
 curl -XPOST ''"$1"':9200/_aliases?pretty' -H 'Content-Type: application/json' -d'
 {
 
