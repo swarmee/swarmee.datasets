960a64ab858572c6745164fd4f92354b875365c7
swarmee <john@swarmee.net>
2017-08-31 22:20:32 +0000

    abc

--- highest-grossing-animated-films/load-using-logstash/highest-grossing-animated-films.sh
+++ highest-grossing-animated-films/load-using-logstash/highest-grossing-animated-films.sh
@@ -1,17 +1,13 @@
 echo '################ delete index '"$2"' on '"$1"'###############'
-curl -XDELETE ''"$1"':9200/highest-grossing-animated-films-'"$2"'?pretty'
+curl -XDELETE ''"$1"':9200/highest-grossing-animated-films-*?pretty'
 echo '################ run load ###################'
-zcat highest-grossing-animated-films.csv.gz  |  HOST=$1 INDEX=$2  /usr/share/logstash/bin/logstash -f highest-grossing-animated-films.conf -b 50 -w 2
-echo '################ Remove Any Old Aliases for highest-grossing-animated-films And Creates a New One for highest-grossing-animated-films-'"$2"' ###############'
+zcat highest-grossing-animated-films.csv.gz  |  HOST=$1 /usr/share/logstash/bin/logstash -f highest-grossing-animated-films.conf -b 50 -w 2
+echo '################ Creates a New One for highest-grossing-animated-films-01-01-2017 ###############'
 curl -XPOST ''"$1"':9200/_aliases?pretty' -H 'Content-Type: application/json' -d'
 {
-
     "actions" : [
-        { "remove" : { "index" : "*", "alias" : "highest-grossing-animated-films" } },
-        { "add" : { "index" : "highest-grossing-animated-films-'"$2"'", "alias" : "highest-grossing-animated-films" } }
-
+        { "add" : { "index" : "highest-grossing-animated-films-01-01-2017, "alias" : "highest-grossing-animated-films" } }
     ]
-
 }
 '
 echo '################ all done  #################'
