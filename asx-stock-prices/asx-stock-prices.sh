0a059b3019eee21a6f00dff327d01b8139feca92
john <john@swarmee.net>
2017-11-26 10:30:32 +1100

    minor updates

--- asx-stock-prices/asx-stock-prices.sh
+++ asx-stock-prices/asx-stock-prices.sh
@@ -1,18 +1,11 @@
-echo '################ delete index ###############'
+#!/bin/bash
+
+# input parameter is required it is the hostname of the elastic cluster you are using 
+
+echo '################ delete existing index ###############'
 curl -XDELETE ''"$1"':9200/asx-stock-prices?pretty' 
-echo '################ run load 1 ###################'
+echo '################ download data data  ###################'
 wget --output-document=asx-stock-prices.csv.gz "https://www.dropbox.com/s/c767gltkq20eo6l/asx-stock-prices.csv.gz?dl=0"
-zcat asx-stock-prices.csv.gz  |  HOST=$1  /usr/share/logstash/bin/logstash -f asx-stock-prices.conf -b 100 -w 2
-echo '################ create alias ###############'
-curl -XPOST ''"$1"':9200/_aliases?pretty' -H 'Content-Type: application/json' -d'
-{
-
-    "actions" : [
-
-        { "add" : { "index" : "asx-stock-prices-2-4-2017", "alias" : "asx-stock-prices" } }
-
-    ]
-
-}
-'
+echo '################ load data  ###################'
+zcat asx-stock-prices.csv.gz  |  HOST=$1  /usr/share/logstash/bin/logstash -f asx-stock-prices.conf -b 100 -w 1
 echo '################ all done  #################'
